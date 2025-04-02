13f5b475dad9cb355b9193a6cf982f37bc081814
Your Name <niko.ramishvili@gmail.com>
2025-04-02 19:47:14 +0400

    5: Add categories section

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('public'))
 
 app.get('/', async (req, res) => {
   async function run() {
